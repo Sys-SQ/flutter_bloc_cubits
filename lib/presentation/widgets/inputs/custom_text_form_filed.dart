4f491ad0b321f4a65268704710c7b3352bfd3c12
sys <sisupeaaivaras@gmail.com>
2024-11-25 00:25:05 -0400

    Final de seccion de la validacion de formularios

---
+++ lib/presentation/widgets/inputs/custom_text_form_filed.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class CustomTextFormFiled extends StatelessWidget {
+  final String? label;
+  final String? hint;
+  final String? errorMessage;
+  final bool obscureText;
+  final Function(String)? onChanged;
+  final String? Function(String?)? validator;
+  const CustomTextFormFiled({
+    super.key,
+    this.label,
+    this.hint,
+    this.errorMessage,
+    this.onChanged,
+    this.validator,
+    this.obscureText = false,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+    final border = OutlineInputBorder(
+      // borderSide: BorderSide(color: colors.primary),
+      borderRadius: BorderRadius.circular(40),
+    );
+    return TextFormField(
+      onChanged: onChanged,
+      validator: validator,
+      obscureText: obscureText,
+      decoration: InputDecoration(
+        enabledBorder: border,
+        focusedBorder:
+            border.copyWith(borderSide: BorderSide(color: colors.primary)),
+        errorBorder:
+            border.copyWith(borderSide: BorderSide(color: Colors.red.shade800)),
+        focusedErrorBorder:
+            border.copyWith(borderSide: BorderSide(color: Colors.red.shade800)),
+        isDense: true,
+        label: label != null ? Text(label!) : null,
+        hintText: hint,
+        errorText: errorMessage,
+        focusColor: colors.primary,
+        // icon:
+        //     Icon(Icons.supervised_user_circle_outlined, color: colors.primary),
+      ),
+    );
+  }
+}
